392469e95b917059855147122b96d40c2b1e4c9a
mohamedosamam7 <138522411+mohamedosamam7@users.noreply.github.com>
2023-07-05 07:57:16 +0300

    Add files via upload

---
+++ find_min_max.py
@@ -0,0 +1,18 @@
+#####################################
+#input my list = [20,30,60,7,120]
+
+#output:
+# smallest num is 7
+#largest num is 120
+######################################
+
+mylist = [20,30,60,7,120]
+
+mylist.sort()
+
+print (mylist)
+
+print("smallest element is : ", mylist[0])
+
+print("largest element is :", mylist[4])
+
